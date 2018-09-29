7ce9a01dfa2364103f0bd0248920d5d42dbec156
Sriraman <1495096@fcps.edu>
2018-09-29 17:46:50 -0400

    my project

--- routes/controller.js
+++ routes/controller.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var Profile = require('../models').profile
-//var Profile = require('../mock/profile')
+// var Profile = require('../models').profile
+var Profile = require('../mock/profile')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendStatus(200)
+  res.send('Hello World')
 });
 
- 
+router.get('/profiles', function (req, res) {
+  res.send(Profile.findAll())
+})
+
+router.get('/profiles/:id', function (req, res) {
+  var profile = Profile.findById(req.params.id)
+  res.send(profile)
+})
+
+router.delete('/profiles/:id', function (req, res) {
+   var profiles = Profile.remove(req.params.id) 
+   res.send(profiles) })
+
+router.post('profiles', function (req, res) {
+  var profiles = Profile.create({ fristName: req.body.fristName, lastName: req.body.lastName, bio: req.body.interests, skills: req.body.skills 
+}) 
+res.send(profiles)
+})
+
+router.get('**', function (req, res, next) { 
+  res.send("this route doesn't exist!")
+});
+
 module.exports = router;
